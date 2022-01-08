5521aeaa88db7485471007fef8fa70c83371dea1
nathan-hawkins <natehawk@live.com>
2022-01-08 00:22:24 -0600

    Small TODO edits.

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,5 +1,6 @@
 import * as Settings from './settings.js';
 
+// TODO - not sure if I'll keep this in the final build or not, but useful for debugging purposes
 export function registerApi()
 {
 	const api = {
