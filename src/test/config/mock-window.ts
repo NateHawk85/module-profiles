882235c11de132c32d1b2ce76413be0d1a0fd593
nathan-hawkins <natehawk@live.com>
2022-05-12 17:25:27 -0500

    Trying to fix the window issue

--- src/test/config/mock-window.ts
+++ src/test/config/mock-window.ts
@@ -1,3 +1,17 @@
+const JSDOMEnvironment = require('jest-environment-jsdom');
+
+module.exports = class CustomizedJSDomEnvironment extends JSDOMEnvironment {
+	constructor(config: any) {
+		super(config);
+		this.global.jsdom = this.dom;
+	}
+
+	teardown() {
+		this.global.jsdom = null;
+		return super.teardown();
+	}
+};
+
 globalThis.window = Object.create(window);
 
 Object.defineProperty(window, 'location', {
